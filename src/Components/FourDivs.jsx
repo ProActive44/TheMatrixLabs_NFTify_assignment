b0c9389d7a2d7d4310cb343b8e47fe656516b8d2
Prasad <prasadmhaske2001@gmail.com>
2023-09-08 19:02:32 +0530

    completed

--- src/Components/FourDivs.jsx
+++ src/Components/FourDivs.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Styles from "../Styles/FourDivs.module.css";
+import info from '../Images/info.png'
+import doller from '../Images/dollar.png'
+import token from '../Images/baseline-token.png'
 
 const FourDivs = ({ data }) => {
   console.log(data);
@@ -10,7 +13,7 @@ const FourDivs = ({ data }) => {
         <div className={Styles.flex}>
           <div>
             <p>Pair Created at</p>
-            <p>Etherium</p>
+            <p>{data.pairCreatedAt}</p>
           </div>
           <div>
             <p>Symbol</p>
@@ -18,60 +21,64 @@ const FourDivs = ({ data }) => {
           </div>
           <div>
             <p>Dex ID</p>
-            <p>#7890</p>
+            <p>{data.dexId}</p>
           </div>
           <div>
             <p>Pair Address</p>
-            <p>#6754</p>
+            <p>{data.pairAddress}</p>
           </div>
         </div>
+        <div className={Styles.circle}><img src={info}/></div>
       </div>
       <div className={Styles.smallContainer}>
         <h3>Base Token</h3>
         <div className={Styles.flex}>
           <div>
             <p>Name</p>
-            <p>Etherium</p>
+            <p>{data.baseToken.name}</p>
           </div>
           <div>
             <p>Symbol</p>
-            <p>ETH</p>
+            <p>{data.baseToken.symbol}</p>
           </div>
           <div>
-            <p>Name</p>
-            <p>#7890</p>
+            <p>Address</p>
+            <p>{data.baseToken.address}</p>
           </div>
         </div>
+        <div className={Styles.circle}><img src={token}/></div>
       </div>
       <div className={Styles.smallContainer}>
         <h3>Quote Token</h3>
         <div className={Styles.flex}>
           <div>
             <p>Name</p>
-            <p>Etherium</p>
+            <p>{data.quoteToken.name}</p>
           </div>
           <div>
             <p>Symbol</p>
-            <p>ETH</p>
+            <p>{data.quoteToken.symbol}</p>
           </div>
           <div>
             <p>Addres</p>
-            <p>#7890</p>
+            <p>{data.quoteToken.address}</p>
           </div>
         </div>
+        <div className={Styles.circle}><img src={token}/></div>
       </div>
       <div className={Styles.smallContainer}>
         <h3>Price</h3>
         <div className={Styles.flex}>
           <div>
             <p>Price Native</p>
-            <p>ETH 7.00</p>
+            <p>{data.priceNative}</p>
           </div>
           <div>
             <p>Price USD</p>
-            <p>1.8m</p>
+            <p>{data.priceUsd}</p>
           </div>
         </div>
+        <div className={Styles.circle}><img src={doller}/></div>
       </div>
     </div>
   );
